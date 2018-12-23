e065e0326069be66f6acd7b56e81ca65280f9229
Andres Perez Alonso <andres93x1@gmail.com>
2018-12-23 18:11:44 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+//RUTAS
+import { APP_ROUTES } from './app.routes';
+
+//MODULOS
+import { PagesModule } from './pages/pages.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PagesModule,
+    SharedModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
